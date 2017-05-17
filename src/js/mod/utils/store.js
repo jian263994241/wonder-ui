a6951c5a6febc0b359e624fac808208cf360936e
jianl <jianjianfengyun@126.com>
2017-05-17 18:07:45 +0800

    ..

--- src/js/mod/utils/store.js
+++ src/js/mod/utils/store.js
@@ -5,7 +5,10 @@ import memoryStorage from 'store/storages/memoryStorage'
 import defaultsPlugin from 'store/plugins/defaults'
 import expirePlugin from 'store/plugins/expire'
 
+const plugins = [defaultsPlugin, expirePlugin];
 
-export const ss = engine.createStore([sessionStorage, memoryStorage], [defaultsPlugin, expirePlugin]);
+export const ss = engine.createStore([sessionStorage, memoryStorage], plugins);
 
-export const ls = engine.createStore([localStorage, memoryStorage], [defaultsPlugin, expirePlugin]);
+export const ls = engine.createStore([localStorage, memoryStorage], plugins);
+
+export const ms = engine.createStore([memoryStorage], plugins);
