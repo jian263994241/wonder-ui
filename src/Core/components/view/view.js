84f6b308bcb84311d4f33f2d0a3a857f49a0ce74
jianl <jianjianfengyun@126.com>
2019-07-22 13:17:23 +0800

    fix history action

--- src/Core/components/view/view.js
+++ src/Core/components/view/view.js
@@ -1,16 +1,24 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { WUI_view } from './styles';
 import { withRouter } from 'react-router-dom';
 import appContext from '../app/appContext';
 import Pages from './PageSwitch';
 
-const View =  withRouter((props) => {
-  const app = useContext(appContext);
+const View =  withRouter(({history, location, ...rest}) => {
+  const app = React.useContext(appContext);
+  const [action, setAction] = React.useState('PUSH');
   const { routes, fallback } = app.params;
+  const search = decodeURIComponent(location.search);
+  const pathname = location.pathname;
+  
+  React.useEffect(()=>{
+    setAction(history.action); 
+  }, [search, pathname])
 
+  
   return (
     <WUI_view>
-      <Pages routes={routes} fallback={fallback} action={props.history.action} {...props}/>
+      <Pages routes={routes} fallback={fallback} action={action} location={location} {...rest}/>
     </WUI_view>
   )
 })
