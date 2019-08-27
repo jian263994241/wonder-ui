d5883bb238e2838987628bd603ccd72de81bb46a
jianl <jianjianfengyun@126.com>
2019-08-27 09:50:07 +0800

    remove

--- src/Core/components/ButtonBase/ButtonBase.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { WUI_button_base } from './styles';
-
-
-const ButtonBase = WUI_button_base;
-
-export default ButtonBase;
--- src/Core/components/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as usePageInit } from './usePageInit';
-export { default as useIsMounted } from './useIsMounted'; 
-export { default as useScreen } from './useScreen';
