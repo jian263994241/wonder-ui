5ba7d895f3f125900f27decaaa3f206814d1752c
jianl <jianjianfengyun@126.com>
2019-11-26 19:50:34 +0800

    rm styles

--- src/styles/useStyles.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import makeStyles from './makeStyles';
-import useTheme from './useTheme';
-
-export default function useStyles(styles){
-  const theme = useTheme();
-  return React.useMemo(()=> makeStyles(styles, theme) , [styles]);
-}
