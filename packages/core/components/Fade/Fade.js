f485be55f7adb2164b6cb8b23187da51dc38f789
jianl <jianjianfengyun@126.com>
2019-12-13 14:21:55 +0800

    update

--- packages/core/components/Fade/Fade.js
+++ packages/core/components/Fade/Fade.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Transition from '../Transition';
 import { easing, duration } from '../styles/transitions';
+import css from 'dom-helpers/css';
 
 const defaultTimeout = {
   enter: duration.enteringScreen,
@@ -11,20 +12,46 @@ const Fade = React.forwardRef((props, ref)=>{
   const {
     timeout = defaultTimeout,
     easing: easingInput = easing.sharp,
+    onEnter,
+    onExited,
     ...rest
   } = props;
+
+  const originStyles = React.useRef({});
+
+  const handleEnter = React.useCallback((node)=>{
+    if(!originStyles.current.display){
+      originStyles.current.display = css(node, 'display');
+    }else{
+      css(node, {display: originStyles.current.display});
+    }
+    onEnter && onEnter();
+  }, []);
+
+  const handleExited = React.useCallback((node)=>{
+   
+    css(node, { display : 'none' });
+    
+    onExited && onExited();
+  }, []);
+
+  const styles = React.useMemo(()=>({ opacity: 0 }), []);
+
+  const targetStyles = React.useMemo(()=>({
+    entering: { opacity: 1 },
+    entered: { opacity: 1 }
+  }), []);
 
   return (
     <Transition
       ref={ref}
-      style={{ opacity: 0 }}
-      styles = {{
-        entering: { opacity: 1 },
-        entered: { opacity: 1 }
-      }}
+      style={styles}
+      styles = {targetStyles}
       propertys={['opacity']}
       timeout={timeout}
       easing={easingInput}
+      onEnter={handleEnter}
+      onExited={handleExited}
       {...rest}
     />
   )
--- packages/core/components/Slide/Slide.js
+++ packages/core/components/Slide/Slide.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Transition from '../Transition';
 import { easing, duration } from '../styles/transitions';
+import css from 'dom-helpers/css';
 
 const defaultTimeout = {
   enter: duration.enteringScreen,
@@ -12,8 +13,26 @@ const Slide = React.forwardRef((props, ref)=>{
   const {
     direction,
     timeout = defaultTimeout,
+    onEnter,
+    onExited,
     ...rest
   } = props;
+
+  const originStyles = React.useRef({});
+
+  const handleEnter = React.useCallback((node)=>{
+    if(!originStyles.current.display){
+      originStyles.current.display = css(node, 'display');
+    }else{
+      css(node, {display: originStyles.current.display});
+    }
+    onEnter && onEnter();
+  }, []);
+
+  const handleExited = React.useCallback((node)=>{
+    css(node, { display : 'none' });
+    onExited && onExited();
+  }, []);
 
   const style = React.useMemo(()=>{
     if(direction === 'left'){
@@ -41,17 +60,22 @@ const Slide = React.forwardRef((props, ref)=>{
     }
   }, [direction]);
 
+  const targetStyles = React.useMemo(()=>({
+    entering: { transform: 'translate3d(0 , 0 , 0)' },
+    entered: { transform: 'translate3d(0 , 0 , 0)' }
+  }), []);
+
+  
   return (
     <Transition
       ref={ref}
       style={style}
-      styles = {{
-        entering: { transform: 'translate3d(0 , 0 , 0)' },
-        entered: { transform: 'translate3d(0 , 0 , 0)' }
-      }}
+      styles = {targetStyles}
       propertys={['transform']}
       easing={easing.sharp}
       timeout={timeout}
+      onEnter={handleEnter}
+      onExited={handleExited}
       {...rest}
     />
   )
