15374ffd9e12df8d943f8f4398ca77f5c83062a2
jianl <jianjianfengyun@126.com>
2020-03-13 11:42:07 +0800

    update styles

--- packages/styles/src/withStyles.js
+++ packages/styles/src/withStyles.js
@@ -1,6 +1,12 @@
 import { withStyles as withStylesWithoutDefault } from 'react-jss';
 import theming from './theming';
 
-export default function withStyles(stylesOrCreator, options) {
-  return withStylesWithoutDefault(stylesOrCreator, { theming, ...options });
+export default function withStyles(stylesOrCreator, options = {}) {
+  const { name, ...rest} = options;
+  return function (StyledComponent){
+    if(name){
+      StyledComponent.displayName = name;
+    }
+    return withStylesWithoutDefault(stylesOrCreator, { theming, ...rest })(StyledComponent)
+  };
 }
