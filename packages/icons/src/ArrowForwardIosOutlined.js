b59d711e769b6b8787f6efa437830d54cd9ee10d
jianl <jianjianfengyun@126.com>
2020-08-21 16:32:01 +0800

    fix: 🐛 fix icons

--- packages/icons/src/ArrowForwardIosOutlined.js
+++ packages/icons/src/ArrowForwardIosOutlined.js
@@ -2,5 +2,6 @@ import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
 
 export default createSvgIcon(
-  <path fill="none" stroke="#000" strokeLinecap="round" strokeLinejoin="round" d="M8 3l8.773 8.773L8 20.547"/>
-, 'ArrowForwardIosOutlined');
+  <path d="M8.146 20.354a.5.5 0 00.638.057l.07-.057 8-8a.5.5 0 00.057-.638l-.057-.07-8-8a.5.5 0 00-.765.638l.057.07L15.793 12l-7.647 7.646a.5.5 0 00-.057.638l.057.07z" />,
+  'ArrowForwardIosOutlined',
+);
--- packages/icons/src/CloseOutlined.js
+++ packages/icons/src/CloseOutlined.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import createSvgIcon from './utils/createSvgIcon';
-//移除
+
 export default createSvgIcon(
-  <path fill="#000" d="M7.05 6.343l4.95 4.95 4.95-4.95a.5.5 0 11.707.707L12.707 12l4.95 4.95a.5.5 0 11-.707.707L12 12.707l-4.95 4.95a.5.5 0 11-.707-.707l4.95-4.95-4.95-4.95a.5.5 0 11.707-.707z"/>
-  ,'CloseOutlined'
-)
+  <path d="M7.05 6.343l4.95 4.95 4.95-4.95a.5.5 0 11.707.707L12.707 12l4.95 4.95a.5.5 0 11-.707.707L12 12.707l-4.95 4.95a.5.5 0 11-.707-.707l4.95-4.95-4.95-4.95a.5.5 0 11.707-.707z" />,
+  'CloseOutlined',
+);
