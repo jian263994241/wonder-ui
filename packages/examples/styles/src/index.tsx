49a47cf718dcaa93bd8d4ee12ea5951df2ba8e05
jianl <jianjianfengyun@126.com>
2020-12-24 19:10:08 +0800

    refactor: 💡 update

---
+++ packages/examples/styles/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+export function App() {
+  return <div>12342</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
