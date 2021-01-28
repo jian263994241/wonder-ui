8ae047d1efd11d3f2efa7bdd2d7c7a8127a10021
jianl <jianjianfengyun@126.com>
2021-01-28 11:20:53 +0800

    chore: remove

--- packages/wonder-ui-styled/src/index.ts
+++ packages/wonder-ui-styled/src/index.ts
@@ -2,3 +2,6 @@ export { default } from '@emotion/styled';
 export { ThemeContext, keyframes } from '@emotion/react';
 export { default as StyledProvider } from './StyledProvider';
 export { default as GlobalStyles } from './GlobalStyles';
+
+//only emotion
+export * from '@emotion/react';
