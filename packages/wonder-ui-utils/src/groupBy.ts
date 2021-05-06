902a311c3568262ffe8b602eb79c194b484926d7
jianl <jianjianfengyun@126.com>
2021-05-06 19:10:39 +0800

    fix: 🐛 update

---
+++ packages/wonder-ui-utils/src/groupBy.ts
@@ -0,0 +1,63 @@
+interface Obj {
+  [prop: string]: any;
+}
+
+interface Func {
+  (...args: any[]): any;
+}
+
+/** Used to check objects for own properties. */
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ * The base implementation of `assignValue` and `assignMergeValue` without
+ * value checks.
+ *
+ * @private
+ * @param {Object} object The object to modify.
+ * @param {string} key The key of the property to assign.
+ * @param {*} value The value to assign.
+ */
+function baseAssignValue(object: Obj, key: any, value: any) {
+  if (key == '__proto__') {
+    Object.defineProperty(object, key, {
+      configurable: true,
+      enumerable: true,
+      value: value,
+      writable: true
+    });
+  } else {
+    object[key] = value;
+  }
+}
+
+/**
+ * Creates an object composed of keys generated from the results of running
+ * each element of `collection` thru `iteratee`. The order of grouped values
+ * is determined by the order they occur in `collection`. The corresponding
+ * value of each key is an array of elements responsible for generating the
+ * key. The iteratee is invoked with one argument: (value).
+ *
+ * @since 0.1.0
+ * @category Collection
+ * @param {Array|Object} collection The collection to iterate over.
+ * @param {Function} iteratee The iteratee to transform keys.
+ * @returns {Object} Returns the composed aggregate object.
+ * @example
+ *
+ * groupBy([6.1, 4.2, 6.3], Math.floor)
+ * // => { '4': [4.2], '6': [6.1, 6.3] }
+ */
+function groupBy(collection: any[], iteratee: Func) {
+  return collection.reduce((result, value, key) => {
+    key = iteratee(value);
+    if (hasOwnProperty.call(result, key)) {
+      result[key].push(value);
+    } else {
+      baseAssignValue(result, key, [value]);
+    }
+    return result;
+  }, {});
+}
+
+export default groupBy;
