de2189f172d2a8e5f5e479c77614a9cd20a5362a
jianl <jianjianfengyun@126.com>
2021-05-12 14:51:22 +0800

    fix: 🐛 update

--- packages/wonder-ui/src/ArrowForward/ArrowForward.tsx
+++ packages/wonder-ui/src/ArrowForward/ArrowForward.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import styled from '../styles/styled';
 import useThemeProps from '../styles/useThemeProps';
-import useClasses from '../styles/useClasses';
-import type { PickStyleProps } from '../styles/types';
 import SvgIcon, { SvgIconProps } from '../SvgIcon';
 
 export interface ArrowForwardProps extends SvgIconProps {
@@ -13,41 +10,27 @@ export interface ArrowForwardProps extends SvgIconProps {
   direction?: 'left' | 'right' | 'up' | 'down';
 }
 
-const StyledSvgIcon = styled(SvgIcon, { name: 'ArrowForward', slot: 'Root' })<
-  PickStyleProps<ArrowForwardProps, 'direction'>
->(({ theme, styleProps }) => ({
-  color: 'inherit',
-  transition: theme.transitions.create(['transform']),
-  transform: {
-    left: 'rotate(180deg)',
-    right: 'rotate(0deg)',
-    up: 'rotate(-90deg)',
-    down: 'rotate(90deg)'
-  }[styleProps.direction]
-}));
-
 const ArrowForward: React.FC<ArrowForwardProps> = React.forwardRef(
   (inProps, ref) => {
     const props = useThemeProps({ props: inProps, name: 'WuiArrowForward' });
 
-    const { size, direction = 'right', className, rootRef, ...rest } = props;
+    const { size, direction = 'right', ...rest } = props;
 
-    const styleProps = { direction };
-    const classes = useClasses({
-      ...props,
-      styleProps,
-      name: 'WuiArrowForward'
-    });
     return (
-      <StyledSvgIcon
-        className={classes.root}
-        size={size}
-        styleProps={styleProps}
-        ref={ref}
-        {...rest}
-      >
-        <path d="M4.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L10.293 8 4.646 2.354a.5.5 0 010-.708z" />
-      </StyledSvgIcon>
+      <SvgIcon size={size} titleAccess="WuiArrowForward" ref={ref} {...rest}>
+        {direction === 'left' && (
+          <path d="M11.354 1.646a.5.5 0 010 .708L5.707 8l5.647 5.646a.5.5 0 01-.708.708l-6-6a.5.5 0 010-.708l6-6a.5.5 0 01.708 0z" />
+        )}
+        {direction === 'right' && (
+          <path d="M4.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L10.293 8 4.646 2.354a.5.5 0 010-.708z" />
+        )}
+        {direction === 'up' && (
+          <path d="M7.646 4.646a.5.5 0 01.708 0l6 6a.5.5 0 01-.708.708L8 5.707l-5.646 5.647a.5.5 0 01-.708-.708l6-6z" />
+        )}
+        {direction === 'down' && (
+          <path d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" />
+        )}
+      </SvgIcon>
     );
   }
 );
