a8517cfb70faa2b43e3ab8d2c2f441bde35b726a
jianl <jianjianfengyun@126.com>
2021-05-12 15:06:02 +0800

    feat: 🎸 add useMount

---
+++ packages/wonder-ui-hooks/src/useMount.ts
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import useEventCallback from './useEventCallback';
+
+export function useMount(fn: () => void) {
+  const [mounted, setMounted] = React.useState(false);
+  const callback = useEventCallback(fn);
+
+  React.useEffect(() => {
+    setMounted(true);
+
+    callback();
+  }, []);
+
+  return mounted;
+}
