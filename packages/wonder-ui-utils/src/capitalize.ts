beeb6f3e4cc0ac602ce4ac1e72628379b8d4d482
jianl <jianjianfengyun@126.com>
2021-05-17 15:03:42 +0800

    feat: 🎸 add methods

--- packages/wonder-ui-utils/src/capitalize.ts
+++ packages/wonder-ui-utils/src/capitalize.ts
@@ -1,11 +1,10 @@
+import upperFirst from './upperFirst';
+import toString from './toString';
+
 /**
  * 首字母大写, 拼接className用
  * @param str
  */
-export default function capitalize(str: string) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Capitalize(string) expects a string argument.');
-  }
-
-  return str.charAt(0).toUpperCase() + str.slice(1);
+export default function capitalize(string: string) {
+  upperFirst(toString(string).toLowerCase());
 }
