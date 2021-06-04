247d50f4b28a8e7e599dd6a58a5f89e0ef676a2d
jianl <jianjianfengyun@126.com>
2021-06-04 18:40:35 +0800

    fix: 🐛 update type

--- packages/wonder-ui-hooks/src/useForkRef.ts
+++ packages/wonder-ui-hooks/src/useForkRef.ts
@@ -1,7 +1,8 @@
 import useEventCallback from './useEventCallback';
 import { setRef } from '@wonder-ui/utils';
+import React from 'react';
 
-export function useForkRef<R extends any[]>(...refs: R) {
+export function useForkRef<T>(...refs: React.Ref<T>[]): React.RefCallback<T> {
   /**
    * This will create a new function if the ref props change and are defined.
    * This means react will call the old forkRef with `null` and the new forkRef
