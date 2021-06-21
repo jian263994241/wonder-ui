ed223702c43fd4b6550abcdd33e12751a81264a2
jianl <jianjianfengyun@126.com>
2021-06-21 16:55:42 +0800

    refactor: 💡 update

--- packages/wonder-ui/src/Divider/demo/vertical.tsx
+++ packages/wonder-ui/src/Divider/demo/vertical.tsx
@@ -2,15 +2,12 @@
  * title: 垂直分割线
  * desc: 使用 `direction="vertical"`设置为行内的垂直分割线。
  */
-/** @jsx jsx */
-import { jsx, Divider, Space } from '@wonder-ui/core';
+import { Divider, Space } from '@wonder-ui/core';
 
-export default function Example() {
-  return (
-    <Space split={<Divider direction="vertical" style={{ height: '1em' }} />}>
-      Text
-      <a href="#">Link</a>
-      <a href="#">Link</a>
-    </Space>
-  );
-}
+export default () => (
+  <Space split={<Divider direction="vertical" style={{ height: '1em' }} />}>
+    Text
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+  </Space>
+);
