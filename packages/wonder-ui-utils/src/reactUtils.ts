918d708fda66235baf02335c4c74c4f01e4c4e9e
jianl <jianjianfengyun@126.com>
2022-03-11 16:48:52 +0800

    feat: 🎸 add  css vars functions

---
+++ packages/wonder-ui-utils/src/reactUtils.ts
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+
+export function renderToBody(element: React.ReactElement) {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  function unmount() {
+    const unmountResult = ReactDOM.unmountComponentAtNode(container);
+    if (unmountResult && container.parentNode) {
+      container.parentNode.removeChild(container);
+    }
+  }
+  ReactDOM.render(element, container);
+  return unmount;
+}
