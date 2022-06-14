c9241040633430d1d44384e6bac2b5da034afe25
jianl <jianjianfengyun@126.com>
2022-06-14 13:53:13 +0800

    fix: 🐛 update doc

--- packages/wonder-ui/src/Result/ResultTypes.ts
+++ packages/wonder-ui/src/Result/ResultTypes.ts
@@ -4,8 +4,20 @@ import { ResultClassesType } from './ResultClasses';
 export interface ResultProps
   extends Omit<React.HTMLAttributes<HTMLDivElement>, 'title'> {
   classes?: Partial<ResultClassesType>;
+  /**
+   * 状态
+   */
   status: 'success' | 'error' | 'info' | 'waiting' | 'warning';
+  /**
+   * 标题
+   */
   title: React.ReactNode;
+  /**
+   * 描述
+   */
   description?: React.ReactNode;
+  /**
+   * 自定义icon
+   */
   icon?: React.ReactNode;
 }
